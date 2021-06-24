ecf405f223a4a9827236235113e1a57a289446fd
Ricky Zhang <haoyuzhangca@outlook.com>
2021-06-24 09:27:35 -0400

    getting ready for 1.0

--- code.ts
+++ code.ts
@@ -1,4 +1,8 @@
-figma.showUI(__html__,{ width: 280, height: 240 })
+figma.showUI(__html__,{ width: 316, height: 420 })
+
+if (figma.currentPage.selection.length > 0) {	
+	figma.ui.postMessage([figma.currentPage.selection[0].width,figma.currentPage.selection[0].height]);
+}
 
 figma.ui.onmessage = sizes => {
 
@@ -8,28 +12,36 @@ figma.ui.onmessage = sizes => {
 	var c = sizes['target-inch'] * mirrorPPI;
 	// target size on mirror vs. actual target size
 	var ratio = c / (Math.sqrt(Math.pow(sizes['target-width'],2) + Math.pow(sizes['target-height'],2)));
-	// a: new target-width, b: new target-height, in pixel
-	var a = sizes['target-width'] * ratio;
-	var b = sizes['target-height'] * ratio;
+	// a: new mirror-width, b: new mirror-height, in pixel
+	var a = sizes['mirror-width'] / ratio;
+	var b = sizes['mirror-height'] / ratio;
 	
 	const nodes: FrameNode[] = [];
 	const mirror = figma.createFrame();
-	mirror.resizeWithoutConstraints(sizes['mirror-width'], sizes['mirror-height'])
+	mirror.resizeWithoutConstraints(a,b)
 	mirror.x = 150;
 	mirror.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}];
 	figma.currentPage.appendChild(mirror);
 	nodes.push(mirror);
-	figma.currentPage.selection = nodes;
-	figma.viewport.scrollAndZoomIntoView(nodes);
+	
+	if (figma.currentPage.selection.length === 0) {
+		const target = figma.createRectangle();
+		target.resizeWithoutConstraints(sizes['target-width'], sizes['target-height'])
+		target.x = Math.round((a - sizes['target-width']) / 2);
+		target.y = Math.round((b - sizes['target-height']) / 2);
+		target.fills = [{type: 'SOLID', color: {r: 0.5, g: 0.5, b: 0.5}}];
+		mirror.appendChild(target);
+	}
+	else {		
+		mirror.x = figma.currentPage.selection[0].x;
+		mirror.y = figma.currentPage.selection[0].y;
+		figma.currentPage.selection[0].x = Math.round((a - sizes['target-width']) / 2);
+		figma.currentPage.selection[0].y = Math.round((b - sizes['target-height']) / 2);
+		mirror.appendChild(figma.currentPage.selection[0]);
+	}
 
-	const target = figma.createRectangle();
-	target.resizeWithoutConstraints(a,b)
-	target.x = 20;
-	target.y = 20;
-	target.fills = [{type: 'SOLID', color: {r: 0.5, g: 0.5, b: 0.5}}];
-	mirror.appendChild(target);
+	figma.currentPage.selection = nodes;
 
 	// close
 	// figma.closePlugin();
 }
-
